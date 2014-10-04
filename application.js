6b49122d7989066fca9debde856393d12ac2d85f
Merrin <merrin.macleod@gmail.com>
2014-10-05 05:06:14 +1300

    first pass at linking everything

---
+++ application.js
@@ -0,0 +1,103 @@
+$( document ).ready(function() {
+  $( '#more-1' ).click(function () {
+    $( '#reveal-1' ).toggleClass('hidden');
+    $( '#more-1' ).toggleClass('clicked');
+  });
+  $( '#more-2' ).click(function () {
+    $( '#reveal-2' ).toggleClass('hidden');
+    $( '#more-2' ).toggleClass('clicked');
+  });
+  $( '#more-3' ).click(function () {
+    $( '#reveal-3' ).toggleClass('hidden');
+    $( '#more-3' ).toggleClass('clicked');
+  });
+  $( '#more-4' ).click(function () {
+    $( '#reveal-4' ).toggleClass('hidden');
+    $( '#more-4' ).toggleClass('clicked');
+  });
+  $( '#more-5' ).click(function () {
+    $( '#reveal-5' ).toggleClass('hidden');
+    $( '#more-5' ).toggleClass('clicked');
+  });
+  $( '#more-6' ).click(function () {
+    $( '#reveal-6' ).toggleClass('hidden');
+    $( '#more-6' ).toggleClass('clicked');
+  });
+  $( '#more-7' ).click(function () {
+    $( '#reveal-7' ).toggleClass('hidden');
+    $( '#more-7' ).toggleClass('clicked');
+  });
+  $( '#more-8' ).click(function () {
+    $( '#reveal-8' ).toggleClass('hidden');
+    $( '#more-8' ).toggleClass('clicked');
+  });
+  $( '#more-9' ).click(function () {
+    $( '#reveal-9' ).toggleClass('hidden');
+    $( '#more-9' ).toggleClass('clicked');
+  });
+  $( '#more-10' ).click(function () {
+    $( '#reveal-10' ).toggleClass('hidden');
+    $( '#more-10' ).toggleClass('clicked');
+  });
+  $( '#more-11' ).click(function () {
+    $( '#reveal-11' ).toggleClass('hidden');
+    $( '#more-11' ).toggleClass('clicked');
+  });
+  $( '#more-12' ).click(function () {
+    $( '#reveal-12' ).toggleClass('hidden');
+    $( '#more-12' ).toggleClass('clicked');
+  });
+  $( '#more-13' ).click(function () {
+    $( '#reveal-13' ).toggleClass('hidden');
+    $( '#more-13' ).toggleClass('clicked');
+  });
+  $( '#more-14' ).click(function () {
+    $( '#reveal-14' ).toggleClass('hidden');
+    $( '#more-14' ).toggleClass('clicked');
+  });
+  $( '#more-15' ).click(function () {
+    $( '#reveal-15' ).toggleClass('hidden');
+    $( '#more-15' ).toggleClass('clicked');
+  });
+  $( '#more-16' ).click(function () {
+    $( '#reveal-16' ).toggleClass('hidden');
+    $( '#more-16' ).toggleClass('clicked');
+  });
+  $( '#more-17' ).click(function () {
+    $( '#reveal-17' ).toggleClass('hidden');
+    $( '#more-17' ).toggleClass('clicked');
+  });
+  $( '#more-18' ).click(function () {
+    $( '#reveal-18' ).toggleClass('hidden');
+    $( '#more-18' ).toggleClass('clicked');
+  });
+  $( '#more-19' ).click(function () {
+    $( '#reveal-19' ).toggleClass('hidden');
+    $( '#more-19' ).toggleClass('clicked');
+  });
+  $( '#more-20' ).click(function () {
+    $( '#reveal-20' ).toggleClass('hidden');
+    $( '#more-20' ).toggleClass('clicked');
+  });
+  $( '#more-21' ).click(function () {
+    $( '#reveal-21' ).toggleClass('hidden');
+    $( '#more-21' ).toggleClass('clicked');
+  });
+  $( '#more-22' ).click(function () {
+    $( '#reveal-22' ).toggleClass('hidden');
+    $( '#more-22' ).toggleClass('clicked');
+  });
+  $( '#more-23' ).click(function () {
+    $( '#reveal-23' ).toggleClass('hidden');
+    $( '#more-23' ).toggleClass('clicked');
+  });
+  $( '#more-24' ).click(function () {
+    $( '#reveal-24' ).toggleClass('hidden');
+    $( '#more-24' ).toggleClass('clicked');
+  });
+  $( '#more-99' ).click(function () {
+    $( '#reveal-99' ).toggleClass('hidden');
+    $( '#more-99' ).toggleClass('clicked');
+  });
+})
+
