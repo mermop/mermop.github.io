f6485591294ab3ee08d2eadd717a47bffe5f0bcc
Merrin <merrin.macleod@gmail.com>
2015-07-29 16:04:07 +1200

    adding images, tested on iPhone 4 safari and fixed for that

--- scripts/select.js
+++ scripts/select.js
@@ -15,6 +15,26 @@ $( document ).on( 'click', '[id^=select]', function() {
   $this.removeClass( 'unselected' );
   $this.addClass( 'selected' );
 
+  update_contact_links();
 
-  console.log(selected + ' class is ' + option )
 });
+
+function update_contact_links () {
+  // create phrase:
+  var phrase;
+  var selected_drink = $('.select-drink.selected').text();
+  var selected_week = $('.select-week.selected').text();
+  if(selected_drink == ""){
+    phrase = "Let's chat "
+  } else {
+    phrase = "Let's talk over " + selected_drink
+  }
+  if(selected_week == ""){
+    phrase = phrase + " sometime."
+  } else {
+    phrase = phrase + " " + selected_week + "."
+  }
+  $('#twitter-link').attr("href", "https://twitter.com/intent/tweet?text=%40merxplat%20" + phrase)
+  $('#email-link').attr("href", "mailto:hello@merr.in?Subject=" + phrase)
+
+}
