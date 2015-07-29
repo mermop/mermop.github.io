8561d797cbcbe3973bd82eda701feaf31c34d9c9
Merrin <merrin.macleod@gmail.com>
2015-07-30 11:46:19 +1200

    tidying up some unused js, target blank for twitter and github links

--- scripts/incomplete_lists.js
+++ scripts/incomplete_lists.js
@@ -1,9 +1,6 @@
 var what_i_do_array = ["Agile project management", "Photography", "Print design", "Editing", "Illustration", "Having great opinions", "Owning many grey markers", "Event management", "Diversity consulting", "I also make great coffee", "Teaching technology", "Telling computers what to do"]
 var talk_to_me_if_array = ["You have a terrible idea.", "You have a great idea.", "You need lean and practical UX advice.", "You just don't know where to use apostrophes and would pay for me to tell you when you are using apostrophes incorrectly.", "You like the cut of my jib.", "You want to talk through and plan a website or app.", "You want to build a small website quickly.", "You are doing interesting work.", "You are doing work that makes the world a little better."]
 
-$( document ).ready(function() {
-});
-
 $( document ).on( 'click', '[id$=_add]', function() {
   var $this = $( this ), // clicked element
   $list = $('#' + this.id.replace( /_add/, '_list' )); // matching 'list'
