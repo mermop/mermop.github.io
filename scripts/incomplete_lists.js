eb7ed3ae7dff4f3718b690ffd05a50d1fe4b3fc2
Merrin <merrin.macleod@gmail.com>
2015-07-29 16:39:33 +1200

    adding open sans

--- scripts/incomplete_lists.js
+++ scripts/incomplete_lists.js
@@ -1,11 +1,10 @@
 var what_i_do_array = ["Agile project management", "Photography", "Print design", "Editing", "Illustration", "Having great opinions", "Owning many grey markers", "Event management", "Diversity consulting?", "Intellectual labour", "Emotional labour", "I also make great coffee", "Telling computers what to do"]
-var talk_to_me_if_array = ["You frequently preface sentences with the phrase 'this might sound crazy, but hear me out'.", "You have a terrible idea.", "You have a great idea.", "You pay bills on time.", "You need a UX consultant who doesn't charge as much as most UX consultants. ", "Maybe you have some kind of dev outreach job? I could totally do that.", "You just don't know where to use apostrophes and will pay for me to tell you when you are using apostrophes incorrectly.", "You like the cut of my jib.", "You will pay me for giving you advice about diversity.", "You will pay me for giving you advice about design.", "You will pay me for making you a website.", "You will pay me for talking through your website idea and making wireframes.", "You will pay me for drawing pictures.", "You are doing interesting work.", "You are doing work that makes the world a little better."]
+var talk_to_me_if_array = ["You frequently preface sentences with the phrase 'this might sound crazy, but hear me out'.", "You have a terrible idea.", "You have a great idea.", "You need a UX consultant who doesn't charge as much as most UX consultants.", "Maybe you have some kind of dev outreach job? I could totally do that.", "You just don't know where to use apostrophes and would pay for me to tell you when you are using apostrophes incorrectly.", "You like the cut of my jib.", "You will pay me for giving you advice about diversity.", "You will pay me for making you a website.", "You want to talk through and plan a website or app.", "You would pay me for drawing pictures.", "You are doing interesting work.", "You are doing work that makes the world a little better."]
 
 $( document ).ready(function() {
 });
 
 $( document ).on( 'click', '[id$=_add]', function() {
-  console.log('hello')
   var $this = $( this ), // clicked element
   $list = $('#' + this.id.replace( /_add/, '_list' )); // matching 'list'
 
